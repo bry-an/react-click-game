f0e719ee864aa484f6bd600590de3ddea1223bc3
bry-an <bbyunis@gmail.com>
2018-10-02 20:31:50 -0600

    state setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import chemicals from '../public/chemicals.json'
+import PictureItem from './components/PictureItem'
 
 class App extends Component {
+  state = {
+    clickedImageIds: [], 
+    score: 0
+  }
+
+  pictureClickHandler = id => {
+    this.setState({
+
+    })
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {chemicals.map(chemical => {
+        <PictureItem 
+      })}
+      
       </div>
     );
   }
